e54ba8307b2495ec9be4ed5d0786071c89ec5991
Isaiah <jackson.isaiah401@gmail.com>
2025-04-09 13:35:54 -0400

    Made a Shopping List (Breakfast Items)

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("ShoppingList: Eggs, Bacon, Sausage, Pancakes, Waffles, Ketchup, Toast, French Toast, Muffins, Sausage Gravy");
+    }
+}
