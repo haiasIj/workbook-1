662f3de8debf4684bb5891ea75b9757356f0c1af
Isaiah <jackson.isaiah401@gmail.com>
2025-04-10 10:16:35 -0400

    struggled to find out whether bob or gary had the highest salary

---
+++ MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+
+        int bobsalary = 54000;
+        int garysalary = 55000;
+        int highestsalary = Math.max(bobsalary,garysalary);
+
+        System.out.println(highestsalary);
+//
+
+        int carprice = 18000;
+        int truckprice = 25000;
+
+        System.out.println();
+    }
+}
